001726e7a3a407d13cd5b7b6648e08fa8b420d9e
Dan Mcley  Pangan <danmcley>
2024-09-03 09:41:34 +0800

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <h1 style="text-align: center">Dan Mcley Pangan</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia impedit reprehenderit consequatur est ipsam itaque numquam quod, enim officiis veniam soluta accusantium id corporis repellendus illum dolores veritatis repudiandae iure?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia impedit reprehenderit consequatur est ipsam itaque numquam quod, enim officiis veniam soluta accusantium id corporis repellendus illum dolores veritatis repudiandae iure?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia impedit reprehenderit consequatur est ipsam itaque numquam quod, enim officiis veniam soluta accusantium id corporis repellendus illum dolores veritatis repudiandae iure?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia impedit reprehenderit consequatur est ipsam itaque numquam quod, enim officiis veniam soluta accusantium id corporis repellendus illum dolores veritatis repudiandae iure?</p>
+
+    </body>
+</html>
