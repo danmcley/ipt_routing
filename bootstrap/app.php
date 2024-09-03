b1443163f691e3a79572c533a3e22595fb7025ef
danmcley <danmcley@gmail.com>
2024-09-03 10:14:20 +0800

    Nay comment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,3 +16,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+    // ian way buot, victor dakog o, yach way mata 
